8bc93643d39d0e33c52f38c66bbc67e7cbd1fa3c
Tyler Allen <tyler.allen@hashicorp.com>
2025-01-16 15:03:55 +0000

    Adding in TF

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # vault-node-kv-testing
+
+## Setting Up Vault Auth
+We will use JWT
+
+### Setup JWT Key
+```bash
+ssh-keygen -t rsa-sha2-256 -b 2048 -f jwt_key -m pem
+openssl rsa -in jwt_key -pubout -outform PEM -out jwt_key.pub
+```
+
+### Setup Vault
+```bash
+export VAULT_ADDR='http://127.0.0.1:8200'
+export VAULT_TOKEN=
+
+terraform init
+
+```
+
---
+++ main.tf
@@ -0,0 +1,42 @@
+provider "vault" {
+
+}
+
+resource "vault_mount" "api" {
+  path    = "api"
+  type    = "kv"
+  options = { version = "2" }
+}
+
+resource "vault_policy" "api-full" {
+  name = "api-full"
+
+  policy = <<EOT
+path "api/*" {
+  capabilities = ["create", "read", "update", "patch", "delete", "list"]
+}
+EOT
+}
+
+resource "vault_jwt_auth_backend" "jwt" {
+  path                   = "jwt"
+  jwt_validation_pubkeys = [file("${path.module}/jwt_key.pub")]
+
+  tune {
+    default_lease_ttl = "1h"
+    max_lease_ttl = "2h"
+  }
+}
+
+resource "vault_jwt_auth_backend_role" "example" {
+  backend        = vault_jwt_auth_backend.jwt.path
+  role_name      = "api"
+  token_policies = ["api-full", "default"]
+
+
+  bound_claims = {
+    app = "api-app-name"
+  }
+  user_claim = "app"
+  role_type  = "jwt"
+}
